fb8b50dacbc763a26aba0fcfa37bd510f255acc6
rafaverde <rafaverde@msn.com>
2024-04-03 19:52:50 -0300

    URL with page and search persistent navigation

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -30,8 +30,25 @@ interface Attendee {
 }
 
 export function AttendeeList() {
-  const [search, setSearch] = useState("")
-  const [page, setPage] = useState(1)
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has("search")) {
+      return url.searchParams.get("search") ?? ""
+    }
+
+    return ""
+  })
+
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString())
+
+    if (url.searchParams.has("page")) {
+      return Number(url.searchParams.get("page"))
+    }
+
+    return 1
+  })
 
   const [total, setTotal] = useState(0)
   const [attendees, setAttendees] = useState<Attendee[]>([])
@@ -57,25 +74,45 @@ export function AttendeeList() {
       })
   }, [page, search])
 
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set("search", search)
+
+    window.history.pushState({}, "", url)
+
+    setSearch(search)
+  }
+
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString())
+
+    url.searchParams.set("page", String(page))
+
+    window.history.pushState({}, "", url)
+
+    setPage(page)
+  }
+
   function OnSearchInputChange(event: ChangeEvent<HTMLInputElement>) {
-    setSearch(event.target.value)
-    setPage(1)
+    setCurrentSearch(event.target.value)
+    setCurrentPage(1)
   }
 
   function goToNextPage() {
-    setPage(page + 1)
+    setCurrentPage(page + 1)
   }
 
   function goToPreviousPage() {
-    setPage(page - 1)
+    setCurrentPage(page - 1)
   }
 
   function goToLastPage() {
-    setPage(totalPages)
+    setCurrentPage(totalPages)
   }
 
   function goToFirstPage() {
-    setPage(1)
+    setCurrentPage(1)
   }
 
   return (
@@ -87,6 +124,7 @@ export function AttendeeList() {
           <Search className="size-4 text-emerald-300" />
           <input
             onChange={OnSearchInputChange}
+            value={search}
             className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0"
             placeholder="Buscar participante..."
             type="text"

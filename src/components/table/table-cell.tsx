223ebea0bc6af5718290c892b636ffc2dd2cdc05
rafaverde <rafaverde@msn.com>
2024-04-03 18:29:46 -0300

    Search implemented

--- src/components/table/table-cell.tsx
+++ src/components/table/table-cell.tsx
@@ -1,6 +1,6 @@
 import { ComponentProps } from "react"
 
-interface TableCellProps extends ComponentProps<"th"> {}
+interface TableCellProps extends ComponentProps<"td"> {}
 
 export function TableCell(props: TableCellProps) {
   return <td className="py-3 px-4 text-sm text-zinc-300" {...props} />
